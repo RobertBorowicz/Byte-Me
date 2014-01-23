8b682c33df3e78096f008d49c0ef540ce3d0e168
Daniel-X-Chen <daniel.xia.chen@gmail.com>
2014-01-23 18:39:00 -0500

    Updated calc method

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -4,7 +4,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  reversed string 
  *  
- *  @author  Bob
+ *  @author  Daniel Chen 
  *  @version 1.1
  */
 public class Person3 {
@@ -42,7 +42,9 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		StringBuffer buffer = new StringBuffer();
+		for (int i=input.length()-1; i>=0; i--) 
+			buffer.append(input.charAt(i));
+		return buffer.toString();
 	}
 }
