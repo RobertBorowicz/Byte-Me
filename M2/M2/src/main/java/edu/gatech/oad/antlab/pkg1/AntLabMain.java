fba6023bbb59b3ecf2853ea9707828377cc8ec14
Nick Adams <nickadams1517@gmail.com>
2014-01-24 14:50:18 -0500

    Nick added his name and GT username to the AntLabMain.java file.

--- M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -67,14 +67,14 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Daniel Chen");
 		  toPrint += p3.toString("dchen80");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
-                  //Person5 replace P5 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("Alex Moran");
-                  toPrint += p5.toString("amoran6");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("Nick Adams");
+          toPrint += p4.toString("nadams33");
+          //Person5 replace P5 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("Alex Moran");
+          toPrint += p5.toString("amoran6");
 		  
         System.out.println(toPrint);
         
